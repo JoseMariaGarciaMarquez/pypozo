0fb3dcd28448aecfd6ea0310c21ed6e6ae7fbf1e
JoseMariaGarciaMarquez <josemariagarciamarquez2.72@gmail.com>
2024-06-03 20:28:28 -0600

    preprocesspozo

--- src/pypozo/aipozo.py
+++ src/pypozo/aipozo.py
@@ -3,4 +3,7 @@ AIpozo
 This module is used to create a class
 """
 
+import numpy as np
 import tensorflow as tf
+import scipy.stats as stats
+import scipy.signal as signal
---
+++ src/pypozo/preprocesspozo.py
@@ -0,0 +1,10 @@
+""""
+Preprocessing data for the model
+"""
+
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import scipy.signal as signal
+
+from sklearn.preprocessing import StandardScaler
